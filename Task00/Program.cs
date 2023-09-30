6e590e591c0e41712a6e227579388ab7efb27e26
Wall-e <you@mail.com>
2023-09-30 16:23:18 +0300

    Reshenie dla vivoda

---
+++ Task00/Program.cs
@@ -0,0 +1,14 @@
+﻿// 
+
+string PrintString (int count, string text)
+{
+    string result = String.Empty;
+    for (int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+
+string res = PrintString (10, "Z");
+Console.WriteLine(res);
